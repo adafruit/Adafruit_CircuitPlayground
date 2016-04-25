b4dec12e4e998f49733b17335f01b8bb4d206c98
ladyada <limor@ladyada.net>
2016-04-25 17:30:31 -0400

    all in one library

---
+++ Adafruit_CircuitPlayground.cpp
@@ -0,0 +1,151 @@
+#include <Adafruit_CircuitPlayground.h>
+
+
+boolean Adafruit_CircuitPlayground::begin(void) {
+  pinMode(CPLAY_REDLED, OUTPUT);
+  pinMode(CPLAY_SLIDESWITCHPIN, INPUT);
+  pinMode(CPLAY_LEFTBUTTON, INPUT);
+  pinMode(CPLAY_RIGHTBUTTON, INPUT);
+  pinMode(CPLAY_BUZZER, OUTPUT);
+  pinMode(CPLAY_CAPSENSE_SHARED, OUTPUT);
+
+  strip = Adafruit_CPlay_NeoPixel(10, CPLAY_NEOPIXELPIN, NEO_GRB + NEO_KHZ800);
+  lis = Adafruit_CPlay_LIS3DH(CPLAY_LIS3DH_CS);
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  strip.setBrightness(20);
+
+  cap[0] = CPlay_CapacitiveSensor(CPLAY_CAPSENSE_SHARED, 0);
+  cap[1] = CPlay_CapacitiveSensor(CPLAY_CAPSENSE_SHARED, 1);
+  cap[2] = CPlay_CapacitiveSensor(CPLAY_CAPSENSE_SHARED, 2);
+  cap[3] = CPlay_CapacitiveSensor(CPLAY_CAPSENSE_SHARED, 3);
+  cap[4] = CPlay_CapacitiveSensor(CPLAY_CAPSENSE_SHARED, 6);
+  cap[5] = CPlay_CapacitiveSensor(CPLAY_CAPSENSE_SHARED, 9);
+  cap[6] = CPlay_CapacitiveSensor(CPLAY_CAPSENSE_SHARED, 10);
+  cap[7] = CPlay_CapacitiveSensor(CPLAY_CAPSENSE_SHARED, 12);
+  
+  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+    return false;
+  }
+  return true;
+}
+
+uint16_t Adafruit_CircuitPlayground::readCap(uint8_t p) {
+  switch (p) {
+  case 0:
+    return cap[0].capacitiveSensor(10);
+  case 1:
+    return cap[1].capacitiveSensor(10);
+  case 2:
+    return cap[2].capacitiveSensor(10);
+  case 3:
+    return cap[3].capacitiveSensor(10);
+  case 6:
+    return cap[4].capacitiveSensor(10);
+  case 9:
+    return cap[5].capacitiveSensor(10);
+  case 10:
+    return cap[6].capacitiveSensor(10);
+  case 12:
+    return cap[7].capacitiveSensor(10);
+  default:
+    return 0;
+  }
+}
+
+
+// just turn on/off the red #13 LED
+void Adafruit_CircuitPlayground::redLED(boolean v) {
+  digitalWrite(CPLAY_REDLED, v);
+}
+
+// just read the slide switch
+boolean Adafruit_CircuitPlayground::slideSwitch(void) {
+  return digitalRead(CPLAY_SLIDESWITCHPIN);
+}
+
+// just read the left button
+boolean Adafruit_CircuitPlayground::leftButton(void) {
+  return digitalRead(CPLAY_LEFTBUTTON);
+}
+
+// just read the right button
+boolean Adafruit_CircuitPlayground::rightButton(void) {
+  return digitalRead(CPLAY_RIGHTBUTTON);
+}
+
+void Adafruit_CircuitPlayground::playTone(uint16_t freq, uint16_t time) {
+  tone(CPLAY_BUZZER, freq, time);
+}
+
+uint16_t Adafruit_CircuitPlayground::lightSensor(void) {
+  return analogRead(CPLAY_LIGHTSENSOR);
+}
+
+uint16_t Adafruit_CircuitPlayground::soundSensor(void) {
+  return analogRead(CPLAY_SOUNDSENSOR);
+}
+
+
+float Adafruit_CircuitPlayground::motionX(void) {
+  sensors_event_t event;
+  CircuitPlayground.lis.getEvent(&event);
+  return event.acceleration.x;
+}
+
+float Adafruit_CircuitPlayground::motionY(void) {
+  sensors_event_t event;
+  CircuitPlayground.lis.getEvent(&event);
+  return event.acceleration.y;
+}
+
+float Adafruit_CircuitPlayground::motionZ(void) {
+  sensors_event_t event;
+  CircuitPlayground.lis.getEvent(&event);
+  return event.acceleration.z;
+}
+
+float Adafruit_CircuitPlayground::temperature(void) {
+   // Thermistor test
+  float reading;
+
+  reading = analogRead(CPLAY_THERMISTORPIN);
+
+  //Serial.print("Thermistor reading: "); Serial.println(reading);
+
+  // convert the value to resistance
+  reading = ((1023.0 * SERIESRESISTOR) / reading);
+  reading -= SERIESRESISTOR;
+
+  //Serial.print("Thermistor resistance: "); Serial.println(reading);
+
+  float steinhart;
+  steinhart = reading / THERMISTORNOMINAL;     // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;                         // convert to C
+
+  return steinhart;
+}
+
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Adafruit_CircuitPlayground::colorWheel(uint8_t WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+// instantiate static
+Adafruit_CircuitPlayground CircuitPlayground;
---
+++ Adafruit_CircuitPlayground.h
@@ -0,0 +1,93 @@
+/*------------------------------------------------------------------------
+  This file is part of the Adafruit Dot Star library.
+
+  Adafruit Dot Star is free software: you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public License
+  as published by the Free Software Foundation, either version 3 of
+  the License, or (at your option) any later version.
+
+  Adafruit Dot Star is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with DotStar.  If not, see <http://www.gnu.org/licenses/>.
+  ------------------------------------------------------------------------*/
+
+#include "utility/Adafruit_CPlay_Neopixel.h"
+#include "utility/Adafruit_CPlay_LIS3DH.h"
+#include "utility/CPlay_CapacitiveSensor.h"
+
+#ifndef _ADAFRUIT_CIRCUITPLAYGROUND_H_
+#define _ADAFRUIT_CIRCUITPLAYGROUND_H_
+
+
+#define CPLAY_REDLED 13
+#define CPLAY_NEOPIXELPIN 17
+#define CPLAY_SLIDESWITCHPIN 21
+#define CPLAY_LEFTBUTTON 4
+#define CPLAY_RIGHTBUTTON 19
+#define CPLAY_LIGHTSENSOR A5
+#define CPLAY_LIS3DH_CS 8
+#define CPLAY_THERMISTORPIN A0
+#define CPLAY_SOUNDSENSOR A4
+#define CPLAY_BUZZER 5
+#define CPLAY_CAPSENSE_SHARED 30
+
+#define SERIESRESISTOR 10000
+// resistance at 25 degrees C
+#define THERMISTORNOMINAL 10000
+// temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25
+
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3950
+// the value of the 'other' resistor
+
+
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+ #include <pins_arduino.h>
+#endif
+
+class Adafruit_CircuitPlayground {
+ public:
+  boolean begin(void);
+
+  Adafruit_CPlay_NeoPixel strip;
+  Adafruit_CPlay_LIS3DH lis;
+  CPlay_CapacitiveSensor cap[8];
+
+  boolean slideSwitch(void);
+  void redLED(boolean v);
+  void playTone(uint16_t freq, uint16_t time);
+  boolean leftButton(void);
+  boolean rightButton(void);
+  uint16_t lightSensor(void);
+  uint16_t soundSensor(void);
+  float temperature(void);
+
+  uint16_t readCap(uint8_t p);
+
+  float motionX(void);
+  float motionY(void);
+  float motionZ(void);
+
+  // neopixels
+  void clearPixels(void) { strip.clear(); strip.show(); }
+  void setPixelColor(uint8_t p, uint32_t c) {strip.setPixelColor(p, c); strip.show();}
+
+  uint32_t colorWheel(uint8_t x);
+ private:
+
+
+};
+
+
+
+extern Adafruit_CircuitPlayground CircuitPlayground;
+
+#endif
