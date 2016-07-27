ae9b761f87709dc41139c275c454b13611e280fb
Phillip Burgess <paintyourdragon@dslextreme.com>
2016-07-27 15:44:24 -0700

    Added say() function from Talkie library

--- utility/Adafruit_CPlay_Speaker.h
+++ utility/Adafruit_CPlay_Speaker.h
@@ -4,13 +4,17 @@
 #ifndef ADAFRUIT_CPLAY_SPEAKER_H
 #define ADAFRUIT_CPLAY_SPEAKER_H
 
+#include <Arduino.h>
+
 class Adafruit_CPlay_Speaker {
  public:
   Adafruit_CPlay_Speaker(void) { started = false; };
   void begin(void),
        end(void),
        set(uint8_t value),
-       playSound(const uint8_t *data, uint16_t length, uint16_t sampleRate);
+       playSound(const uint8_t *data, uint16_t length, uint16_t sampleRate),
+       say(const uint8_t *address);
+
  private:
   boolean started;
 };
