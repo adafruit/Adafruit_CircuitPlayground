0a838f0ae28e06e1ff97d1501cff78096aaa9375
Mike Barela <mydigitalhome@home>
2016-08-19 19:32:39 +0200

    Add examples
    Add sound examples from Adafruit Learning System: Circuit Playground
    Sound and Music

---
+++ examples/sound_music/CPtheme/CPtheme.ino
@@ -0,0 +1,82 @@
+// Adafruit Circuit Playground - Theme Song    Support Open Source, buy at Adafruit
+//   2016-08-06 Version 1  by Mike Barela for Adafruit Industries
+//   Adapted from melody by Tom Igoe on arduino.cc
+// Uses the CircuitPlayground library to easily use the full functionality of the board
+
+#include <Adafruit_CircuitPlayground.h>
+#include "pitches.h"
+
+int melody[] = {                            // specific notes in the melody
+NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0  
+ };
+int numNotes; // Number of notes in the melody
+
+int noteDurations[] = {     // note durations
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
+
+void setup() {
+  CircuitPlayground.begin();              // initialize the CP library
+  numNotes = sizeof(melody)/sizeof(int);  // number of notes we are playing
+}
+
+void loop() {
+  if(CircuitPlayground.rightButton()) {   // play when we press the right button
+    for (int thisNote = 0; thisNote < numNotes; thisNote++) { // play notes of the melody
+      // to calculate the note duration, take one second divided by the note type.
+      int noteDuration = 1000 / noteDurations[thisNote];
+      CircuitPlayground.playTone(melody[thisNote], noteDuration);
+
+      // to distinguish the notes, set a minimum time between them.
+      //   the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+    }
+  }
+}
