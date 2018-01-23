255c1bfe1c97735daaaf539e82cb3e88a913340a
caternuson <methuzla@speakeasy.net>
2018-01-23 07:53:11 -0800

    updated to work with CPX

--- examples/Hello_CircuitPlayground/Hello_CapTouch/Hello_CapTouch.ino
+++ examples/Hello_CircuitPlayground/Hello_CapTouch/Hello_CapTouch.ino
@@ -1,6 +1,10 @@
 #include <Adafruit_CircuitPlayground.h>
 
-uint8_t pads[] = {3, 2, 0, 1, 12, 6, 9, 10};
+// Circuit Playground Express pads
+uint8_t CPX_pads[] = {A1, A2, A3, A4, A5, A6, A7};
+
+// Circuit Playground Classic pads
+uint8_t CPC_pads[] = {3, 2, 0, 1, 12, 6, 9, 10};
 
 void setup() {
   Serial.begin(9600);
@@ -8,11 +12,24 @@ void setup() {
 }
 
 void loop() {
-  for (int i=0; i<8; i++) {
-    Serial.print(CircuitPlayground.readCap(pads[i]));
-    Serial.print("\t");
+  if (CircuitPlayground.isExpress()) {
+    //
+    // Circuit Playground Express
+    //
+    for (int i=0; i<7; i++) {
+      Serial.print(CircuitPlayground.readCap(CPX_pads[i]));
+      Serial.print("\t");    
+    }
+  } else {
+    //
+    // Circuit Playground Classic
+    //    
+    for (int i=0; i<8; i++) {
+      Serial.print(CircuitPlayground.readCap(CPC_pads[i]));
+      Serial.print("\t");    
+    }
   }
+  
   Serial.println();
-   
   delay(1000);
 }
